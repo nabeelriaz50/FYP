fc315c7b4f7a4673dc6358e47d87946594ebd451
nabeelriaz50 <nabeelriaz48@yahoo.com>
2016-10-24 15:42:42 +0500

    First FYP Push

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player; 
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+    public float speed = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x*speed, 0, z*speed) * Time.deltaTime;
+
+        Vector3 newPosition = new Vector3();
+        newPosition = transform.position + movement;
+        GetComponent<Rigidbody>().MovePosition(newPosition);
+
+	}
+}
